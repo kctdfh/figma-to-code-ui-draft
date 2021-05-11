7ecf1a8d80df3fd73e51e513cd32a3497a0c1f45
Khashayar Toodehfallah <khashayar@toodehfallah.com>
2021-05-11 16:13:29 -0400

    horizontal scroll code block

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   mode: 'jit',
   purge: [
     "index.html",
-    "code-bug.html"
+    "code-bug.html",
+    "code-bug-horizontal-scroll.html"
   ],
   darkMode: false,
   theme: {
